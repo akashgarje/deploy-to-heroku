39dc377aaebe2405307377b458a00154af5f0574
Akash Garje <akash15484@mastek.com>
2022-04-15 17:22:17 +0530

    First commit, added REST API to access items, stores and user authentication

---
+++ app.py
@@ -0,0 +1,39 @@
+from flask import Flask
+from flask_restful import Api
+from security import authenticate, identity
+from resources.user import UserRegister
+from resources.item import Item, ItemList
+from resources.store import Store, StoreList
+from db import db
+# from flask_jwt import JWT, jwt_required
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.secret_key = 'akash'
+api = Api(app)
+
+
+@app.before_first_request
+def create_tables():
+    db.create_all()
+
+# jwt = JWT(app, authenticate, identity)        # /auth
+
+
+# First REST Api app:
+# class Student(Resource):
+#     def get(self, name):
+#         return {'student':name}
+
+# api.add_resource(Student, '/student/<string:name>')         #http://127.0.0.1:5000/student/Akash
+
+api.add_resource(Store, '/store/<string:name>')
+api.add_resource(Item, '/item/<string:name>')
+api.add_resource(StoreList, '/stores')
+api.add_resource(ItemList, '/items')
+api.add_resource(UserRegister, '/register')
+
+if __name__ == '__main__':
+    db.init_app(app)
+    app.run(port=5000, debug=True)
